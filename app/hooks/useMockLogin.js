4cd6684cc372695f19072aa51ff033dc300d19a7
rana <rana.buddy@gmail.com>
2025-04-10 13:19:51 +0600

    feat:first commit

--- app/hooks/useMockLogin.js
+++ app/hooks/useMockLogin.js
@@ -26,7 +26,7 @@ function useMockLogin(adminId, posterId) {
       console.log("success", data);
       Cookies.set("email", data?.info?.email);
       Cookies.set("id", data?.info?._id);
-      router.push("/map");
+      router.push("/security-check");
     } else {
       console.log("error", data);
       toast.error("Something Went Wrong");
